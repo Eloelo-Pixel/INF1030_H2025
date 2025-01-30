ae7ae0f2affcc83a0c087cd88a73683c681a6751
occhibel <lily.occhibelli@uqtr.ca>
2025-01-30 10:12:16 -0500

    first commit

---
+++ app/src/main/java/com/example/inf1030_h2025/Connexion.java
@@ -0,0 +1,18 @@
+package com.example.inf1030_h2025;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Connexion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion);
+        } ;
+    }
